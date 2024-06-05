cdbc3899a2b240e694a5d1e90df3855494e06546
ihakki <117304461+ihc006@users.noreply.github.com>
2024-06-06 00:10:19 +0300

    Delete etop directory

--- etop/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE SCHEMA "organization-service";
-ALTER SCHEMA "organization-service" OWNER TO postgres;
-CREATE SCHEMA "user-service";
-ALTER SCHEMA "user-service" OWNER TO postgres;
-CREATE SCHEMA "test-centre-service";
-ALTER SCHEMA "test-centre-service" OWNER TO postgres;
